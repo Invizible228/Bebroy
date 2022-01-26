d5fe3b4f5527b7902faf6bf1551f90f9b05b667e
Invizible228 <Sulaymanov_tima@mail.ru>
2022-01-26 21:36:21 +0500

    master

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -10,13 +10,13 @@ switch (ans)
     case 1:
         Console.WriteLine($"Ответ:{a + b}");
         break;
-    //case 2:
-    //    Console.WriteLine($"Ответ:{a - b}");
-    //    break;
-    //case 3:
-    //    Console.WriteLine($"Ответ:{a * b}");
-    //    break;
-    //case 4:
-    //    Console.WriteLine($"Ответ:{a / b}");
-    //    break;
+    case 2:
+        Console.WriteLine($"Ответ:{a - b}");
+        break;
+    case 3:
+        Console.WriteLine($"Ответ:{a * b}");
+        break;
+    case 4:
+        Console.WriteLine($"Ответ:{a / b}");
+        break;
 }
